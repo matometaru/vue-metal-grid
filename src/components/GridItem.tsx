e12b50239d4617bc80f11bd72b31bd906d17b8d7
matometaru <matometaru@gmail.com>
2019-08-24 18:16:00 +0900

    D: 不要な$propsの記述を削除

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -57,8 +57,8 @@ export default Vue.extend({
       appearTimer: null,
       node: null,
       state: {
-        ...getPositionStyles(this.$props.rect, 1, this.$props.rtl),
-        ...getTransitionStyles('appear', this.$props),
+        ...getPositionStyles(this.rect, 1, this.rtl),
+        ...getTransitionStyles('appear', this),
       },
     };
   },
@@ -67,21 +67,18 @@ export default Vue.extend({
     rect() {
       this.setStateIfNeeded({
         ...this.state,
-        ...getPositionStyles(this.$props.rect, 2, this.$props.rtl),
+        ...getPositionStyles(this.rect, 2, this.rtl),
       });
     },
   },
 
   mounted() {
-    setTimeout(
-      this.setAppearedStyles,
-      this.$props.appearDelay * this.$props.index
-    );
-    this.$props.mountedCb(this);
+    setTimeout(this.setAppearedStyles, this.appearDelay * this.index);
+    this.mountedCb(this.$props);
   },
 
   destroyed() {
-    this.$props.unmountCb(this);
+    this.unmountCb(this.$props);
   },
 
   methods: {
@@ -92,32 +89,32 @@ export default Vue.extend({
     setAppearedStyles() {
       this.setStateIfNeeded({
         ...this.state,
-        ...getTransitionStyles('appeared', this.$props),
-        ...getPositionStyles(this.$props.rect, 1, this.$props.rtl),
+        ...getTransitionStyles('appeared', this),
+        ...getPositionStyles(this.rect, 1, this.rtl),
       });
     },
 
     setEnterStyles() {
       this.setStateIfNeeded({
         ...this.state,
-        ...getPositionStyles(this.$props.rect, 2, this.$props.rtl),
-        ...getTransitionStyles('enter', this.$props),
+        ...getPositionStyles(this.rect, 2, this.rtl),
+        ...getTransitionStyles('enter', this),
       });
     },
 
     setEnteredStyles() {
       this.setStateIfNeeded({
         ...this.state,
-        ...getTransitionStyles('entered', this.$props),
-        ...getPositionStyles(this.$props.rect, 1, this.$props.rtl),
+        ...getTransitionStyles('entered', this),
+        ...getPositionStyles(this.rect, 1, this.rtl),
       });
     },
 
     setLeaveStyles() {
       this.setStateIfNeeded({
         ...this.state,
-        ...getPositionStyles(this.$props.rect, 2, this.$props.rtl),
-        ...getTransitionStyles('leaved', this.$props),
+        ...getPositionStyles(this.rect, 2, this.rtl),
+        ...getTransitionStyles('leaved', this),
       });
     },
 
@@ -178,7 +175,7 @@ export default Vue.extend({
       const styles = this.getStyles();
       // elにスタイル設定。
       this.setStyles(el, styles);
-      setTimeout(done, this.$props.duration);
+      setTimeout(done, this.duration);
     },
     /* eslint-enable no-unused-vars */
   },
