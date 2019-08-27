92b4a42771c902e3762df23f6e202959c78a082b
matometaru <matometaru@gmail.com>
2019-08-27 22:21:53 +0900

    A: add Data type

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,15 @@ type Rect = {
   width: number;
   height: number;
 };
+
+type Size = {
+  width: number;
+  height: number;
+};
+
+type Layout = {
+  rects: Rect[];
+  actualWidth: number;
+  height: number;
+  columnWidth: number;
+};
