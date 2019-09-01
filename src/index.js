1fe1c1e825aeddf99046c5c8056d6e4e807f1eb7
matometaru <matometaru@gmail.com>
2019-09-01 14:15:15 +0900

    D: index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import MetalGrid from './components/MetalGrid';
-
-// export * as easings from './animations/easings';
-// export * as transitions from './animations/transitions/';
-
-export default MetalGrid;
